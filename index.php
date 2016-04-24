d4958b6ddad59b15effc8aba38e9b4722afa56f0
Michael <mjyara9@misena.edu.co>
2016-04-23 20:13:55 -0500

    Introduccion

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Git</title>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <h1 class="titulo">Hola mundo </h1>
+</body>
+</html>
